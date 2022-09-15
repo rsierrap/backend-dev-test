6f0edcd8656ab1f5795a094715735cc6e459ccc5
Raúl Sierra Pajares <rsierra@mobbeel.com>
2022-09-15 22:59:37 +0200

    improvement: add needed changes to return a "not found" response

--- src/main/java/com/challenge/backenddevtest/controller/ApiController.java
+++ src/main/java/com/challenge/backenddevtest/controller/ApiController.java
@@ -1,10 +1,14 @@
 package com.challenge.backenddevtest.controller;
 
+import com.challenge.backenddevtest.response.NotFoundResponse;
 import com.challenge.backenddevtest.model.ProductDetail;
 import com.challenge.backenddevtest.service.SimilarProductService;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,13 @@ public class ApiController {
     @GetMapping("/product/{productId}/similar")
     public ResponseEntity<Set<ProductDetail>> getSimilarProducts(@PathVariable String productId) {
         return ResponseEntity.ok(similarProductService.getSimilarProducts(productId));
+    }
+
+    @ExceptionHandler(FeignException.NotFound.class)
+    public ResponseEntity<NotFoundResponse> returnNotFoundProduct() {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(NotFoundResponse.builder().description("Product Not Found").build());
     }
 
 }
---
+++ src/main/java/com/challenge/backenddevtest/response/NotFoundResponse.java
@@ -0,0 +1,10 @@
+package com.challenge.backenddevtest.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class NotFoundResponse {
+    private String description;
+}
